0726187fecc6bff319b5939330b11837813d3920
Habib Okanla <habib.okanla@gmail.com>
2016-05-14 10:57:51 -0700

    Added sample app

---
+++ sample/src/main/java/com/habibokanla/sample/FabEvent.java
@@ -0,0 +1,15 @@
+package com.habibokanla.sample;
+
+import android.view.View;
+
+/**
+ * 2016
+ * Created by habibokanla on 14/05/16.
+ */
+public class FabEvent {
+    public final View view;
+
+    public FabEvent(View view) {
+        this.view = view;
+    }
+}
---
+++ sample/src/main/java/com/habibokanla/sample/MainActivity.java
@@ -0,0 +1,74 @@
+package com.habibokanla.sample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.habibokanla.navi.Listen;
+import com.habibokanla.navi.Navi;
+
+public class MainActivity extends AppCompatActivity {
+
+    Navi navi = new Navi();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navi.post(new FabEvent(view));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        navi.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        navi.unRegister(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Listen
+    public void onFabEvent(FabEvent event) {
+        Snackbar.make(event.view, "Hey, Listen!", Snackbar.LENGTH_LONG).show();
+    }
+}
